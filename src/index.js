896c9d56004e61126fe4e565ece1912ab7b59b3e
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2022-12-28 13:39:57 +0530

    minor change changed from tygor to chowther

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1>Hello World!</h1>
-    <p>hoy tygor</p>
+    <p>hoy chowther</p>
   </div>,
   document.getElementById("root")
 );
