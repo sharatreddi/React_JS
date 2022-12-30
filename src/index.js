d0d4168aadde7eb869772472c3f35a5c940d10c6
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2022-12-30 23:17:20 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 //Created inline styles, and also declared an obj and called it in heading inline style
-
 import React from "react";
 import ReactDOM from "react-dom";
 
-// ---------------------main part--------------------
-// we create inline Styles, we shld give "" coz in JSON, it takes as a string
-// also we create an object here and then call it in inline styles
-const customStyle = {
-  color: "blue",
-  fontSize: "25px",
-  border: "1px solid black"
-};
+// const date = new Date(2022, 30, 12, 12);
+const currtime = new Date().getHours();
+console.log(currtime);
 
+let greeting;
+let customStyle;
+//let custStyle = { color = "" }
+
+if (currtime < 12) {
+  greeting = "Good Morning";
+  customStyle = {
+    color: "red"
+  };
+  //custStyle.color="red"
+} else if (currtime > 12 && currtime < 18) {
+  greeting = "Good Afternoon";
+  customStyle = {
+    color: "green"
+  };
+} else if (currtime > 18 && currtime < 24) {
+  greeting = "Good Evng";
+  customStyle = {
+    color: "blue"
+  };
+}
 ReactDOM.render(
   <div>
-    <h1 style={customStyle}>My Fav things</h1>
+    <h2 style={customStyle}>{greeting}</h2>
   </div>,
   document.getElementById("root")
 );
