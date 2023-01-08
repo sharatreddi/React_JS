260b047c8bedbdb61a0e94e32e78f9b21dcf6a7d
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-08 22:03:18 +0530

    initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="Ronaldo" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.phn_no}</p>
-        <p className="info">{props.email}</p>
+        <Detail detailInfo={props.email} />
+        <Detail detailInfo={props.phn_no} />
       </div>
     </div>
   );
