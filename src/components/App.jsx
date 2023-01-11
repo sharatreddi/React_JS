52d46b219f356d0de3b2dffdbcfcecb05c4cb2b1
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-11 20:00:41 +0530

    added arrow function added arrow function to emojee function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,6 @@
 import React from "react";
-import Heading from "./Heading";
-import Card from "./Card";
-import List from "./List";
-import contacts from "../contacts";
 import Emojipedia from "../emojipedia";
 import Emoji from "./Emoji";
-
-// function createCard(contact) {
-//   return (
-//     <Card
-//       key={contact.id}
-//       key={contact.id}
-//       name={contact.name}
-//       img={contact.imgURL}
-//       email={contact.email}
-//       phn_no={contact.phone}
-//     />
-//   );
-// }
-
-function createEmoji(emojee) {
-  return (
-    <Emoji
-      key={emojee.id}
-      emoji={emojee.emoji}
-      name={emojee.name}
-      meaning={emojee.meaning}
-    />
-  );
-}
 
 function App() {
   return (
@@ -38,26 +10,14 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        {Emojipedia.map(createEmoji)}
-        {/* Idhi seperate code, code before doin practice thingy, ila kindha prathidhaniki raskokunda map fn use chesi cheyyachu ane dhanki idhi example 
-        <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        phn_no={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        phn_no={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        phn_no={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
+        {Emojipedia.map((emojee) => (
+          <Emoji
+            key={emojee.id}
+            emoji={emojee.emoji}
+            name={emojee.name}
+            meaning={emojee.meaning}
+          />
+        ))}
       </dl>
     </div>
   );
