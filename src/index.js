4eee4b14eac5cd607bceb9b01daf2634f23409fe
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-11 19:48:39 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import Card from "./components/Card";
-// ------------------------------------------what happened anedhi(react dev tools)---------------------------------------
-//so ikkad em jarigindh ante, I mapped data to componentd
-// ------------------------------------------what happened in mapping data to components---------------------------------------
-//Here, in this, we used map function, it is used to parse each and every element of an array(basically designed fot it)
-//This map basically takes a function as an input, and that happened to be our createEmoji function in app.jsx which calls emoji.jsx and every
-//element in emojipedia.js is rendered.
+// ------------------------------------------what happened anedhi(functions)---------------------------------------
+//so ikkad em jarigindh ante, used functions, look in for more details, all of them are commented out,
+//uncomment wot all u wann see
 
-ReactDOM.render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById("root")
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+// function double(x) {
+//   return x * 2;
+// }
+// const newNumbers = numbers.map(double);
+
+// var newNumbers = [];
+// numbers.forEach(function (x) {
+//   newNumbers.push(x * 2);
+// });
+
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
+
+// console.log(newNumbers);
+
+// Filter - Create a new array by keeping the items that return true.
+
+// const newNumbers = numbers.filter((num) => num < 10);
+
+// console.log(newNumbers);
+
+// var newNumbers = [];
+// numbers.forEach(function(num) {
+//   if (num < 10) {
+//     newNumbers.push(num);
+//   }
+// })
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+// var newNumber = numbers.reduce((accumulator, currentNumber)=> accumulator + currentNumber
+//console.log("accumulator = " + accumulator);
+//console.log("currentNumber = " + currentNumber);
+// )
+
+// var newNumber = 0;
+// numbers.forEach(function (currentNumber) {
+//   newNumber += currentNumber
+// })
+
+// console.log(newNumber);
+
+//Find - find the first item that matches from an array.
+
+// const newNumber = numbers.find((num) => num > 10)
+
+// console.log(newNumber);
+
+//FindIndex - find the index of the first item that matches.
+
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// })
+
+// console.log(newNumber);
+
+//Challenge Solution
+import emojipedia from "./emojipedia";
+
+const newEmojipedia = emojipedia.map((emojiEntry) =>
+  emojiEntry.meaning.substring(0, 100)
 );
+
+console.log(newEmojipedia);
