75c817a63af886db7c5b2bd495dbadbe43222aaa
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-22 20:41:24 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-// ------------------------------------------what happened in Event handling of react---------------------------------------
-//Here, I dealt with some event handling of react
-//created a button, when it is hovered, turns black and white when not, here i did it using event handlers, inline styles, react hooks, use states
-//maintainin the state of our react code, we deal with event listeners, using ternary operations, go thru the code and if any doubts, refer vid 425.
+// ------------------------------------------what happened in react forms---------------------------------------
+//Here, I dealt with forms in react
+//what happened in this is I kept in some event handlers for input like onChange and etc
+//also included another event handler of button, together which makes it take the input and display the name when button is pressed
+//then for form, they refreshes(makin send, post reqs and all) everytym u press submit, to prevent this, we take the event handler of button out of it and place in form
+//we had the control of that event now, so we use event.preventDefault which prevents refreshing
+//look at the last of vid 426 for more clarity
 ReactDOM.render(
   <div>
     <App />
