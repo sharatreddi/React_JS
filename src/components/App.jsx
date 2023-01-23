d2ba111bb2ba6586c0f2ac7edbd2892627414ecc
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-23 12:07:49 +0530

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
 
-function App() {
-  // functions and states for setting that hover button of submit
-
-  const [ismouseover, setmouseover] = useState(false);
-  function Mouseover() {
-    setmouseover(true);
+class App extends React.Component {
+  render() {
+    return <ClassComponent />;
   }
-
-  function Mouseout() {
-    setmouseover(false);
-  }
-  // functions and states for displaying name after hello
-  const [name, setName] = useState("");
-  function handleChange(event) {
-    console.log(event.target.value);
-    setName(event.target.value);
-  }
-
-  const [headingText, setheadingText] = useState("");
-  function clicked(event) {
-    setheadingText(name);
-
-    event.preventDefault();
-  }
-
-  return (
-    <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={clicked}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-        />
-        <button
-          type="submit"
-          style={{ backgroundColor: ismouseover ? "black" : "white" }}
-          onMouseOver={Mouseover}
-          onMouseOut={Mouseout}
-        >
-          {" "}
-          Submit{" "}
-        </button>
-      </form>
-    </div>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 // ------------------------------------------what happened in react forms---------------------------------------
-//Here, I dealt with forms in react
-//what happened in this is I kept in some event handlers for input like onChange and etc
-//also included another event handler of button, together which makes it take the input and display the name when button is pressed
-//then for form, they refreshes(makin send, post reqs and all) everytym u press submit, to prevent this, we take the event handler of button out of it and place in form
-//we had the control of that event now, so we use event.preventDefault which prevents refreshing
-//look at the last of vid 426 for more clarity
-ReactDOM.render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById("root")
-);
+//this is abt diff between class components and functional components, how class components have bigger syntax
+//how react developers got pissed off with all "this" statement usages in classes and else
+//why they introduced hooks and they can be used in functional components and all
+//compare both class component and functional component and know the diff, and also realise how irritating class components can be sometimes
+//look at the last of vid 427 for more clarity
+
+ReactDOM.render(<App />, document.getElementById("root"));
