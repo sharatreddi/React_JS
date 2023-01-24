2e5d9fdeda7637be446f9dbd9ffd2cb6aa9f09d6
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-24 19:28:03 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-// ------------------------------------------what happened in react forms---------------------------------------
-//Here, I dealt with forms in react
-//what happened in this is I kept in some event handlers for input like onChange and etc
-//also included another event handler of button, together which makes it take the input and display the name when button is pressed
-//then for form, they refreshes(makin send, post reqs and all) everytym u press submit, to prevent this, we take the event handler of button out of it and place in form
-//we had the control of that event now, so we use event.preventDefault which prevents refreshing
-//look at the last of vid 426 for more clarity
+// ------------------------------------------what happened in react changing complex state---------------------------------------
+//described everything in the app.jsx, go there and have a look
+//look at the last of vid 428 for more clarity
+//this is all abt changing complex state in react, will add its practice in the next branch
+//feels like uploading this part as a branch too, 4 more clarity ig :))
 ReactDOM.render(
   <div>
     <App />
