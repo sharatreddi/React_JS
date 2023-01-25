c45427c5e154af963d28b2d0e0ca3981d0323168
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-25 20:04:13 +0530

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,51 +17,15 @@ function App() {
     lname: ""
   });
 
-  // function handleChange(event) {
-  //   console.log(event.target.value);
-  //   const newValue = event.target.value;
-  //   const inputname = event.target.name;
-  //   setfullname((prevValue) => {
-  //     if (inputname === "fname") {
-  //       return {
-  //         fname: newValue,
-  //         lname: prevValue.lname
-  //       };
-  //     } else if (inputname === "lname") {
-  //       return {
-  //         fname: prevValue.fname,
-  //         lname: newValue
-  //       };
-  //     }
-  //   });
-  // }
-
-  //we can minimise the above handle change event into much simpler code
   function handleChange(event) {
-    console.log(event.target.value);
-    // here we destructured the object(event.target),
-    // such that name corresponds to the input's name that is triggered and value is the value we enter in it
-
     const { value, name } = event.target;
-    /*---------------------------------------------------A warning---------------------------------------------------------*/
-    // in the future, do not try to access an event in the stateful setups (i.e setfullname function here)
-    //for example instead of gettin the name here in the bottom, if we try event.target.name, it triggers the "synthetic events reused" error in the console
-    //refer vid 428 last for more clarity
-    //refer the above code, if in case u did not understand thisbelow handlechange event
-    //this one is a replica of the above code, but just destructured the event.target into respective things
     setfullname((prevValue) => {
-      if (name === "fname") {
-        return {
-          fname: value,
-          lname: prevValue.lname
-        };
-      } else if (name === "lname") {
-        return {
-          fname: prevValue.fname,
-          lname: value
-        };
-      }
+      return {
+        ...prevValue,
+        [name]: value
+      };
     });
+    console.log(value);
   }
 
   const [headingText, setheadingText] = useState("");
--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-// ------------------------------------------what happened in react changing complex state---------------------------------------
-//described everything in the app.jsx, go there and have a look
-//look at the last of vid 428 for more clarity
-//this is all abt changing complex state in react, will add its practice in the next branch
-//feels like uploading this part as a branch too, 4 more clarity ig :))
+// ------------------------------------------what happened in javascript ES6 spread operator---------------------------------------
+//The spread operator is a new addition to the set of operators in JavaScript ES6. It takes in an iterable (e.g an array) and expands it into individual elements.
+// The spread operator is commonly used to make deep copies of JS objects.
+// put 3 dots before the required array or object, and that'll be included in another array/object
+//written down is some of the operation using spread operator
+//also changed the whole if things in app component, used spread operator, have a look
 ReactDOM.render(
   <div>
     <App />
   </div>,
   document.getElementById("root")
 );
+
+const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", "Guava", ...citrus, "Mango"];
+
+console.log(fruits);
+
+const players = {
+  Madrid: "Ronaldo",
+  Barca: "Messi"
+};
+
+const baller = {
+  ...players,
+  Barcelona: "Neymar"
+};
+
+console.log(baller);
