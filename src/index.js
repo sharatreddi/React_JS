fbbe057ef1c165e13df04c7b7d444d91ed7cf235
Sharat Reddi <110130577+Cherry0777@users.noreply.github.com>
2023-01-27 00:05:29 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 // ------------------------------------------what happened in react changing complex state---------------------------------------
-//described everything in the app.jsx, go there and have a look
-//look at the last of vid 428 for more clarity
-//this is all abt changing complex state in react, will add its practice in the next branch
-//feels like uploading this part as a branch too, 4 more clarity ig :))
-ReactDOM.render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
